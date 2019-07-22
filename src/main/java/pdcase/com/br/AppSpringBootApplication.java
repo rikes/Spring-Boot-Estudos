618c25f82acdf5eaf5b32a15028940b313d7efc4
Henrique Santana <henricke.ss@gmail.com>
2019-07-21 22:57:10 -0300

    Initial commit

---
+++ src/main/java/pdcase/com/br/AppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pdcase.com.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringBootApplication.class, args);
+	}
+
+}
